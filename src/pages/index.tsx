270d74f27f71ff81e9af895819c051dd0d137963
eniodev <enioblvck@gmail.com>
2024-10-01 16:06:32 +0100

    feat: basic items search

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import useSWR from 'swr'
 import { Product } from "@/product";
 import { Card } from "./components/Card";
+import { useEffect, useRef, useState } from "react";
 
 const fetcher = (...args: any) => fetch(...args).then((res) => res.json());
 
@@ -20,18 +21,50 @@ const geistMono = localFont({
 
 export default function Home() {
   const { data, error } = useSWR('/api/products', fetcher);
-
+  const [filteredItems, setFilteredItems] = useState<Product[]>([])
+  const searchRef = useRef<HTMLInputElement>(null);
+  
+  const searchItems = async () => {
+    if(searchRef.current) {
+      const searchValue = searchRef.current.value.trim();
+      const _filteredItems = await data.products
+            .filter((item: Product) => item.name.toLowerCase().includes(searchValue.toLowerCase()))
+      setFilteredItems(_filteredItems);
+    }
+    else setFilteredItems(data.products)
+  }
+  
   return (
     <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+      className={`${geistSans.variable} ${geistMono.variable} flex flex-col items-start justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
-      <main className="grid grid-cols-3 gap-8 row-start-2 items-center sm:items-start">
-        {data && (
-          data.products.map((product: Product) => (
-            <Card {...product} />
-          ))
-        )
-      }
+      <header className="">
+        <h1 className="text-lg font-bold">Fetch store.</h1>
+      </header>
+      <main className="flex flex-col w-full h-full">
+        <div className="flex mb-20">
+          <input 
+            className="w-full gap-52 pl-2 bg-transparent border-[0.1px] focus:outline-none"
+            type="search" 
+            placeholder="Looking for?" 
+            ref={searchRef}
+            onChange={searchItems}
+            />
+            <select name="price-filter" className="bg-transparent ml-5 focus:outline-none">
+            <option value="">All</option>
+            <option value="desc">Lowest Price</option>
+            <option value="asc">Highest Price</option>
+            </select>
+        </div>
+        <div className="grid grid-cols-3 gap-8 row-start-2 items-center sm:items-start">
+          {filteredItems.length ? (
+            filteredItems.map((product: Product) => (
+              <Card {...product} />
+            ))
+            ):
+            <p className="col-span-3 text-sm text-center">Nothing to show here...</p>
+          }
+        </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
